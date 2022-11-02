9ffe9ea12ac48aae74d507caa05d84a8294edf55
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2022-11-02 13:30:32 +0100

    build

---
+++ build/precache-manifest.547df437702b82f9d5de04f433c8030b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c5db756f12fbd9bfe8",
+    "url": "/static/css/main.a787e697.chunk.css"
+  },
+  {
+    "revision": "01c5db756f12fbd9bfe8",
+    "url": "/static/js/main.01c5db75.chunk.js"
+  },
+  {
+    "revision": "2f08d85548be314aec29",
+    "url": "/static/js/1.2f08d855.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "547dacda226a0a16dbec890a48424f82",
+    "url": "/index.html"
+  }
+];
